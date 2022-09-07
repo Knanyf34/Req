fea06781b5991ea225135c1500d6503c732a51db
ALMAS <knanyf34@gmail.com>
2022-09-07 12:33:51 +0430

    add main

---
+++ main.py
@@ -0,0 +1,14 @@
+import requests as requests
+url = 'https://jsonplaceholder.typicode.com/todos/2'
+response = requests\
+    .get(url)
+print(type(response))
+print(response)
+print(response.status_code)
+print(response.text)
+print(response.text[0])
+print(type(response.json()))
+print(response.json()['userId'])
+print(response.json()['id'])
+print(response.json()['title'])
+print(response.json()['completed'])
